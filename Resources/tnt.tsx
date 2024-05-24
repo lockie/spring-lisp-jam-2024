ae6d59e961a27b51110bb47a756f8baec090eeb3
Andrew Kravchuk <awkravchuk@gmail.com>
2024-05-24 17:27:38 +0200

    Added some missing resource files

---
+++ Resources/tnt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="tnt" tilewidth="64" tileheight="64" tilecount="189" columns="21">
+ <image source="Factions/Goblins/Troops/TNT/Red/TNT_Red.png" width="1344" height="576"/>
+</tileset>
