4e6732493c69711a8d15eafe6bd4d26c4948c43e
Andrew Kravchuk <awkravchuk@gmail.com>
2024-05-27 01:52:16 +0200

    Added win & lose sound effects

--- src/progress.lisp
+++ src/progress.lisp
@@ -4,6 +4,10 @@
 (ecs:defsystem test-win
   (:components-ro (map)
    :when (length= 0 (team 0)))
+  (make-sound-effect -1 :win
+                     (/ +window-width+ 2.0)
+                     (/ +window-height+ 2.0)
+                     :variations 1)
   (ecs:delete-entity entity)
   (setf *current-map* -1)
   (toggle-ui-window :win-message :on t))
@@ -11,6 +15,10 @@
 (ecs:defsystem test-defeat
   (:components-ro (map)
     :when (length= 0 (team 1)))
+  (make-sound-effect -1 :lose
+                     (/ +window-width+ 2.0)
+                     (/ +window-height+ 2.0)
+                     :variations 1)
   (ecs:delete-entity entity)
   (setf *current-map* -1)
   (toggle-ui-window :loose-message :on t))
