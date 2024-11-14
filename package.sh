002e991f0047ec719c4b94c95b4a8144ea84234f
Andrew Kravchuk <awkravchuk@gmail.com>
2024-11-14 20:08:27 +0100

    Tweaks to build system

--- package.sh
+++ package.sh
@@ -30,7 +30,9 @@ case $1 in
         do_build
         
         ntldd -R bin/* | grep ucrt64 | awk -F '=> ' '{ print $2 }' | awk '{ print $1 }' | xargs -I deps cp deps bin
-        
+
+        convert package/icon.png -define icon:auto-resize=16,32,48,64,256 "$TEMP/icon.ico"
+        convert -resize 150x57 -extent 150x57 -gravity center -background white -alpha remove -alpha off package/icon.png "BMP2:$TEMP/icon.bmp"
         makensis package/installer.nsi
         ;;
 
